a42518e55ba729b37c0eadf363dbce4070ab4faf
Steven <stevenms@outlook.com>
2020-10-29 23:17:48 +0000

    list-rendering

--- src/main.js
+++ src/main.js
@@ -6,7 +6,19 @@ const app = Vue.createApp({
             image: './assets/images/socks_blue.jpg',
             url: 'https://www.bbc.co.uk/news',
             inventory: 5,
-            onSale: true
+            onSale: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: '2234', colour: 'green' },
+                { id: '2235', colour: 'blue' }
+            ],
+            sizes: [ 
+                { id: '3001', name: 'XS' },
+                { id: '3002', name: 'S' },
+                { id: '3003', name: 'M' },
+                { id: '3004', name: 'L' },
+                { id: '3005', name: 'XL' }
+            ]
         }
     }
 })
