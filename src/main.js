d6b91bbeafe6dc3b8eb3a69b9ae3ac2884e97bf5
Steven <stevenms@outlook.com>
2020-11-01 20:11:39 +0000

    event-handling

--- src/main.js
+++ src/main.js
@@ -1,16 +1,17 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Weed',
+            product: 'Socks',
             description: 'This is a description',
             image: './assets/images/socks_blue.jpg',
             url: 'https://www.bbc.co.uk/news',
-            inventory: 5,
-            onSale: true,
+            inventory: 100,
+            onSale: false,
+            cart: 0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: '2234', colour: 'green' },
-                { id: '2235', colour: 'blue' }
+                { id: '2234', colour: 'green', image: './assets/images/socks_green.jpg' },
+                { id: '2235', colour: 'blue', image: './assets/images/socks_blue.jpg' }
             ],
             sizes: [ 
                 { id: '3001', name: 'XS' },
@@ -19,6 +20,17 @@ const app = Vue.createApp({
                 { id: '3004', name: 'L' },
                 { id: '3005', name: 'XL' }
             ]
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1;
+        },
+        removeFromCart() {
+            if(this.cart > 0) this.cart -= 1;
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
         }
     }
 })
